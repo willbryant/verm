e148c4f3de2fca37bb5c4071d85ed01dea111d2a
Will Bryant <will.bryant@gmail.com>
2011-11-13 23:02:54 +1300

    swapped some test support code around, no functional change in this commit

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -30,6 +30,21 @@ module Verm
       10 # seconds
     end
     
+    def get(options)
+      http = Net::HTTP.new(VERM_SPAWNER.hostname, VERM_SPAWNER.port)
+      http.read_timeout = timeout
+      
+      response = http.get(options[:path], options[:headers])
+      
+      assert_equal options[:expected_response_code] || 200, response.code.to_i, "The response didn't have the expected code"
+      assert_equal options[:expected_content_type], response.content_type, "The response had an incorrect content-type" if options.has_key?(:expected_content_type)
+      assert_equal options[:expected_content_length], response.content_length, "The response had an incorrect content-length" if options.has_key?(:expected_content_length)
+      assert_equal options[:expected_content_encoding], response['content-encoding'], "The response had an incorrect content-encoding" if options.has_key?(:expected_content_encoding)
+      assert_equal options[:expected_content], response.body, "The response had incorrect content" if options.has_key?(:expected_content)
+      
+      response
+    end
+    
     def post_file(options)
       orig_filename = File.join(File.dirname(__FILE__), 'fixtures', options[:file])
       file_data = File.read(orig_filename)
@@ -64,21 +79,6 @@ module Verm
       raise "The data saved to verm doesn't match the original! #{saved_data.inspect} vs. #{file_data.inspect}" unless saved_data == file_data
       raise "The data was saved to #{location}, but it was supposed to be saved under #{options[:path]}/" if location[0..options[:path].length] != "#{options[:path]}/"
       dest_filename
-    end
-    
-    def get(options)
-      http = Net::HTTP.new(VERM_SPAWNER.hostname, VERM_SPAWNER.port)
-      http.read_timeout = timeout
-      
-      response = http.get(options[:path], options[:headers])
-      
-      assert_equal options[:expected_response_code] || 200, response.code.to_i, "The response didn't have the expected code"
-      assert_equal options[:expected_content_type], response.content_type, "The response had an incorrect content-type" if options.has_key?(:expected_content_type)
-      assert_equal options[:expected_content_length], response.content_length, "The response had an incorrect content-length" if options.has_key?(:expected_content_length)
-      assert_equal options[:expected_content_encoding], response['content-encoding'], "The response had an incorrect content-encoding" if options.has_key?(:expected_content_encoding)
-      assert_equal options[:expected_content], response.body, "The response had incorrect content" if options.has_key?(:expected_content)
-      
-      response
     end
   end
 end
