0f7c6ebd63895b40e3c776272529c3447bb9bdae
Will Bryant <will.bryant@gmail.com>
2011-12-30 11:40:13 +1300

    extract some MHD state reporting methods

---
+++ src/mhd_patches.h
@@ -0,0 +1,13 @@
+#ifndef MHD_PATCHES_H
+#define MHD_PATCHES_H
+
+#include "microhttpd.h"
+
+const char *MHD_connection_get_http_version(struct MHD_Connection* connection);
+const char *MHD_connection_get_method(struct MHD_Connection* connection);
+const char *MHD_connection_get_url(struct MHD_Connection* connection);
+int MHD_connection_has_response(struct MHD_Connection* connection);
+unsigned int MHD_connection_get_response_code(struct MHD_Connection* connection);
+uint64_t MHD_connection_get_response_bytes_sent(struct MHD_Connection* connection);
+
+#endif
--- src/response_logging.c
+++ src/response_logging.c
@@ -1,6 +1,6 @@
 #include "response_logging.h"
 
-#include "src/daemon/internal.h"
+#include "mhd_patches.h"
 
 #ifdef NEED_SA_LEN
 	#include <sys/un.h>
@@ -25,14 +25,12 @@
 	#define SA_LEN(x) x->sa_len
 #endif
 
-#define REQUEST_FAILED(connection) (connection->responseCode < 200 || connection->responseCode >= 400)
-
 static pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 static struct LogStatistics statistics;
 
 int responded(struct MHD_Connection* connection) {
-	return (connection->response != NULL);
+	return MHD_connection_has_response(connection);
 }
 
 int log_response(struct MHD_Connection* connection, int suppress_log_output, int statistics_request, int new_file_stored) {
@@ -43,21 +41,24 @@ int log_response(struct MHD_Connection* connection, int suppress_log_output, int
 	char timebuf[32];
 	int result = 0;
 
+	unsigned int responseCode = MHD_connection_get_response_code(connection);
+	const char* method = MHD_connection_get_method(connection);
+
 	if (pthread_mutex_lock(&log_mutex) < 0) return -1;
 
-	if (strcmp(connection->method, "GET") == 0) {
+	if (strcmp(method, "GET") == 0) {
 		if (!statistics_request) statistics.get_requests++;
-		if (connection->responseCode == MHD_HTTP_NOT_FOUND) statistics.get_requests_not_found++;
+		if (responseCode == MHD_HTTP_NOT_FOUND) statistics.get_requests_not_found++;
 
-	} else if (strcmp(connection->method, "POST") == 0) {
+	} else if (strcmp(method, "POST") == 0) {
 		statistics.post_requests++;
 		if (new_file_stored) statistics.post_requests_new_file_stored++;
-		if (REQUEST_FAILED(connection)) statistics.post_requests_failed++;
+		if (responseCode < 200 || responseCode >= 400) statistics.post_requests_failed++;
 
-	} else if (strcmp(connection->method, "PUT") == 0) {
+	} else if (strcmp(method, "PUT") == 0) {
 		statistics.put_requests++;
 		if (new_file_stored) statistics.put_requests_new_file_stored++;
-		if (REQUEST_FAILED(connection)) statistics.put_requests_failed++;
+		if (responseCode < 200 || responseCode >= 400) statistics.put_requests_failed++;
 	}
 
 	if (!suppress_log_output) {
@@ -67,7 +68,14 @@ int log_response(struct MHD_Connection* connection, int suppress_log_output, int
 		localtime_r(&now, &now_tm);
 		strftime(timebuf, sizeof(timebuf), "%d/%b/%Y:%H:%M:%S %z", &now_tm); // standard CLF time format
 
-		result = fprintf(stdout, "%s - - [%s] \"%s %s %s\" %d %ju\n", addr, timebuf, connection->method, connection->url, connection->version, connection->responseCode, (uintmax_t)connection->response_write_position);
+		result = fprintf(stdout, "%s - - [%s] \"%s %s %s\" %u %ju\n",
+			addr,
+			timebuf,
+			MHD_connection_get_method(connection),
+			MHD_connection_get_url(connection),
+			MHD_connection_get_http_version(connection),
+			MHD_connection_get_response_code(connection),
+			(uintmax_t)MHD_connection_get_response_bytes_sent(connection));
 	}
 
 	if (pthread_mutex_unlock(&log_mutex) < 0) return -1;
