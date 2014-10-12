1f4025c23975a2d28948435022b9fe11c13fe324
Will Bryant <will.bryant@gmail.com>
2014-10-12 17:50:38 +1300

    remove all the code from the c version

--- src/statistics_reports.h
+++
@@ -1,9 +0,0 @@
-#ifndef STATISTICS_REPORTS_H
-#define STATISTICS_REPORTS_H
-
-#include "microhttpd.h"
-
-char *create_log_statistics_string(struct MHD_Connection *connection, const char *suffix);
-char *create_statistics_string(struct MHD_Connection *connection);
-
-#endif
