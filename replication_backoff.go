4930beaef5e443276bfa5b30deaa7b7f717dcbfa
Will Bryant <will.bryant@gmail.com>
2015-03-04 20:15:46 +1300

    make backoff times partly randomized to avoid thundering herds

--- replication_backoff.go
+++ replication_backoff.go
@@ -1,5 +1,6 @@
 package main
 
+import "math/rand"
 import "time"
 
 func backoffTime(failures uint) time.Duration {
@@ -8,6 +9,7 @@ func backoffTime(failures uint) time.Duration {
 	}
 
 	backoffTime := ReplicationBackoffBaseDelay*(2 << (failures - 3))
+	backoffTime = backoffTime/2 + int(rand.Int63n(int64(backoffTime)/2))
 
 	if backoffTime > ReplicationBackoffMaxDelay {
 		return ReplicationBackoffMaxDelay*time.Second
